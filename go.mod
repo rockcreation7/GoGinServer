bcdcfac51640b4587c0d07006c6a893f7eb2df7f
rockcreation7 <rockcreation7@gmail.com>
2020-08-17 01:15:36 +0800

    add func return success json, and folder path

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module goGinServer
 go 1.15
 
 require (
+	github.com/gin-gonic/gin v1.6.3
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
-	github.com/olahol/go-imageupload v0.0.0-20160503070439-09d2b92fa05e // indirect
+	github.com/olahol/go-imageupload v0.0.0-20160503070439-09d2b92fa05e
 )
--- main.go
+++ main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/olahol/go-imageupload"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.File("index.html")
+	})
+
+	r.POST("/upload", func(c *gin.Context) {
+		img, err := imageupload.Process(c.Request, "file")
+
+		if err != nil {
+			panic(err)
+		}
+
+		err = img.Save("upload/" + img.Filename)
+
+		if err != nil {
+			panic(err)
+		}
+
+		c.JSON(200, gin.H{
+			"message": "uploadSuccess",
+		})
+	})
+	r.Run(":4000")
+}
